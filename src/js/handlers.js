c0011d10e6d9a16a0e942f8bde1c4e3f69ba3731
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-09-09 02:49:44 +0200

    new code in js

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,35 +1,35 @@
 import { loadPhotos } from './load.js';
 
 export async function searchForPhotos(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-e.target.page.value = '1';
-const q = e.target.q.value;
+  e.target.page.value = '1';
+  const q = e.target.q.value;
 
-await loadPhotos ({ q, page: '1' });
+  await loadPhotos({ q, page: '1' });
 }
 
 export async function scrollHandler() {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight) {
-        const searchForm = document.querySelector('#searchPhotosForm');
-        const page = parseInt(searchForm.page.value);
-        searchForm.page.value = page + 1;
-        await loadPhotos({ q: searchForm.q.value, page: searchForm.page.value});
-    }
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    const searchForm = document.querySelector('#searchPhotosForm');
+    const page = parseInt(searchForm.page.value);
+    searchForm.page.value = page + 1;
+    await loadPhotos({ q: searchForm.q.value, page: searchForm.page.value });
+  }
 }
 
 export function enlargePhoto(e) {
     // Check if the clicked element is an image
     if (e.target.classList.contains('photo')) {
-        const enlargedPhotoContainer = document.querySelector('#enlargedPhotoContainer');
-        enlargedPhotoContainer.innerHTML = '';
-
-    const enlargedPhoto = document.createElement('img');
-    enlargedPhoto.src = e.target.src;
-    enlargedPhoto.classList.add('enlargedPhoto');
-
-    enlargedPhotoContainer.appendChild(enlargedPhoto);
-    enlargedPhotoContainer.style.display = 'flex';
-}
-}
+      const enlargedPhotoContainer = document.getElementById('modal-image'); // Update the container ID here
+      enlargedPhotoContainer.innerHTML = '';
+  
+      const enlargedPhoto = document.createElement('img');
+      enlargedPhoto.src = e.target.src;
+      enlargedPhoto.classList.add('enlargedPhoto');
+  
+      enlargedPhotoContainer.appendChild(enlargedPhoto);
+      enlargedPhotoContainer.style.display = 'flex';
+    }
+  }
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,49 @@
-import { searchForPhotos, scrollHandler } from './handlers.js';
-
-const searchForm = document.querySelector('#searchPhotosForm');
-
-searchForm.addEventListener('submit', searchForPhotos);
-searchForm.dispatchEvent(new Event('submit'));
-
-scrollHandler();
-window.addEventListener('scroll', scrollHandler);
-
-const images = document.querySelectorAll('.image');
-
-images.forEach(image => {
-  image.addEventListener('mouseenter', () => {
-    image.style.transform = 'scale(1.2)';
+import {
+    searchForPhotos,
+    scrollHandler,
+    enlargePhoto
+  } from './handlers.js';
+  
+  const searchForm = document.querySelector('#searchPhotosForm');
+  
+  searchForm.addEventListener('submit', searchForPhotos);
+  // Uncomment the line below if you want to load photos on page load
+  // searchForm.dispatchEvent(new Event('submit'));
+  
+  scrollHandler();
+  window.addEventListener('scroll', scrollHandler);
+  
+  const photosContainer = document.querySelector('#photos');
+  photosContainer.addEventListener('click', enlargePhoto);
+  
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+  const closeModal = document.getElementById('close-modal');
+  
+  const photos = document.querySelectorAll('.photo'); // Move the declaration and initialization here
+  
+  photos.forEach(photo => {
+    photo.addEventListener('click', () => {
+      modal.style.display = 'block';
+      modalImage.src = photo.src;
+    });
   });
-
-  image.addEventListener('mouseleave', () => {
-    image.style.transform = 'scale(1)';
+  
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
   });
-});
+  
+  // Function to handle left and right infinite scroll
+  function handleInfiniteScroll(e) {
+    const { scrollLeft, scrollWidth, clientWidth } = e.target;
+    if (scrollLeft + clientWidth >= scrollWidth - 10) {
+      const searchForm = document.querySelector('#searchPhotosForm');
+      const page = parseInt(searchForm.page.value);
+      searchForm.page.value = page + 1;
+      searchForPhotos(e); // Trigger a new photo search on scroll
+    }
+  }
+  
+  // Add event listeners for infinite scroll
+  photoContainer.addEventListener('scroll', handleInfiniteScroll);
+  photoContainer.addEventListener('wheel', handleInfiniteScroll);
